0f64e077ce86aad4c3bd3e2e1097e6e4de5644e0
Moshe Zada <moshe@forter.com>
2016-09-17 22:13:52 +0300

    Basic infra

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='elasticsearch_loader',
+    version='0.1',
+    py_modules=['elasticsearch_loader'],
+    url='',
+    license='',
+    description=''
+)
