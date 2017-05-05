91fad32ff1a7b70d19c54accf86cfb7195b175b5
Moshe Zada <zada.moshe@gmail.com>
2017-05-05 20:25:19 +0300

    Use bulk api instead of home made threads

--- test/test_csv.py
+++ test/test_csv.py
@@ -4,7 +4,7 @@ import mock
 
 
 def invoke(*args, **kwargs):
-    content = """id,first,last\nMOZA,Moshe,Zada\nMICHO,Michelle,Obama"""
+    content = """id,first,last\nMOZA,Moshe,Zada\nMICHO,Michelle,Obama\na,b,c\nf,g,h"""
     runner = CliRunner()
     with runner.isolated_filesystem():
         with open('sample.csv', 'w') as f:
@@ -14,16 +14,12 @@ def invoke(*args, **kwargs):
 
 @mock.patch('elasticsearch_loader.single_bulk_to_es')
 def test_should_iterate_over_csv(bulk):
-        result = invoke(cli, ['--index=index', '--type=type', 'csv', 'sample.csv'], catch_exceptions=False)
-        assert result.exit_code == 0
-        assert filter(None, bulk.call_args[0][0]) == ({'first': 'Moshe', 'id': 'MOZA', 'last': 'Zada'}, {'first': 'Michelle', 'id': 'MICHO', 'last': 'Obama'})
-
-
-@mock.patch('elasticsearch_loader.single_bulk_to_es')
-def test_should_iterate_over_csv_concurrency_1(bulk):
-        result = invoke(cli, ['--concurrency=1', '--index=index', '--type=type', 'csv', 'sample.csv'], catch_exceptions=False)
-        assert result.exit_code == 0
-        assert filter(None, bulk.call_args[0][0]) == ({'first': 'Moshe', 'id': 'MOZA', 'last': 'Zada'}, {'first': 'Michelle', 'id': 'MICHO', 'last': 'Obama'})
+    result = invoke(cli, ['--index=index', '--type=type', 'csv', 'sample.csv'], catch_exceptions=False)
+    assert result.exit_code == 0
+    assert filter(None, bulk.call_args[0][0]) == ({'first': 'Moshe', 'id': 'MOZA', 'last': 'Zada'},
+                                                  {'first': 'Michelle', 'id': 'MICHO', 'last': 'Obama'},
+                                                  {'first': 'b', 'id': 'a', 'last': 'c'},
+                                                  {'first': 'g', 'id': 'f', 'last': 'h'})
 
 
 @mock.patch('elasticsearch_loader.single_bulk_to_es')
@@ -31,4 +27,4 @@ def test_should_iterate_over_csv_bulk_size_1(bulk):
         result = invoke(cli, ['--bulk-size=1', '--index=index', '--type=type', 'csv', 'sample.csv'], catch_exceptions=False)
         assert result.exit_code == 0
         assert bulk.call_count == 2
-        assert filter(None, bulk.call_args[0][0]) == ({'first': 'Michelle', 'id': 'MICHO', 'last': 'Obama'},)
+        assert filter(None, bulk.call_args[0][0]) == ({'first': 'g', 'id': 'f', 'last': 'h'},)
