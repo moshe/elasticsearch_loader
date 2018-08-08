f0bc4f4bb1e1699c311da5d31301177fd06de519
Moshe Zada <zada.moshe@gmail.com>
2018-08-08 22:49:00 +0300

    Specify minimum elasticsearch client

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except Exception:
 setup(
     name='elasticsearch-loader',
     author='Moshe Zada',
-    version='0.2.6',
+    version='0.2.7',
     packages=['elasticsearch_loader'],
     keywords=['elastic', 'elasticsearch', 'csv', 'json', 'parquet', 'bulk', 'loader'],
     url='https://github.com/Moshe/elasticsearch_loader',
